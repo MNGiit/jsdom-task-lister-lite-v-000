d45567dcfdecaaab6d2c092797552f3477833cd1
Mohammed Noori <kmanoorib@gmail.com>
2020-02-24 02:21:28 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const taskForm = document.getElementById("create-task-form");
   taskForm.addEventListener("submit", function() {
-    
+    console.log("button was clicked");
   });
   
   
