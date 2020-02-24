581fdea931ff3f121a4cd756a644647b9fd90db4
Mohammed Noori <kmanoorib@gmail.com>
2020-02-24 02:39:15 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -22,18 +22,3 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-
-class Task {
-  constructor(description) {
-    this.description = description;
-  }
-
-  render() {
-    return `
-      <li>
-        ${this.description}
-        <button data-description="${this.description}">X</button>
-      </li>
-      `;
-  }
-}
