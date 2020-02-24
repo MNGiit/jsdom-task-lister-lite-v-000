ad9ec5c951d6c9a354eff61f75283eeee1818de2
Mohammed Noori <kmanoorib@gmail.com>
2020-02-24 02:39:11 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,16 +2,38 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const taskForm = document.getElementById("create-task-form");
   const newTask = document.getElementById("new-task-description");
+  let tasks = [];
   taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
     // alert("button was clicked");
     
     function addTask(task) {
-      alert(task);
+      // alert(task);
+      tasks.push(task);
+      taskString(task);
     }
-    // addTask(newTask.value);
+    function taskString(task) {
+      return `<li>${task}</li>`;
+    }
+    addTask(newTask.value);
+    
+    
   });
 });
 
 
 
+class Task {
+  constructor(description) {
+    this.description = description;
+  }
+
+  render() {
+    return `
+      <li>
+        ${this.description}
+        <button data-description="${this.description}">X</button>
+      </li>
+      `;
+  }
+}
